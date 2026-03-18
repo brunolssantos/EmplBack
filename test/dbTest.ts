57a20b540ac0afe903fca45ac3491cb0ef7b212c
Bruno Santos <brunolssantos@hotmail.com>
2026-03-18 13:50:28 -0300

    Create new methods in the repository to access the database using copilot.

--- test/dbTest.ts
+++ test/dbTest.ts
@@ -2,7 +2,16 @@ import { EmplRepository } from "../src/repository/EmplRepository";
 
 async function main() {
     const emplRepository = new EmplRepository();
-    const employees = await emplRepository.getAllEmployees();
+    // const employees = await emplRepository.addEmployee({
+    //     firstName: "John",
+    //     lastName: "Doe",
+    //     hireDate: new Date(),
+    //     position: "Manager"
+    // });
+    // console.log(employees);
+
+    const employees = await emplRepository.getEmployees();
+
     console.log(employees);
 }
 
